d25d697dc86006dc37f52ea23fcb5e04a3b5487a
Hari Vishnu Parashar <harryskotch11@gmail.com>
2023-05-03 21:30:04 +0530

    feat: init

---
+++ src/services/firebase/config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAk7TfI0qu923KW--ew03P3zOuqWkfId8g",
+  authDomain: "lucyai-83a55.firebaseapp.com",
+  projectId: "lucyai-83a55",
+  storageBucket: "lucyai-83a55.appspot.com",
+  messagingSenderId: "1083822965482",
+  appId: "1:1083822965482:web:1ca6ea0b26c357e6f84d93",
+  measurementId: "G-R4026H586X",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const analytics = getAnalytics(app);
