fe785c89a45cdfae55ff63d91718a0206979ca84
Hari Vishnu Parashar <harryskotch11@gmail.com>
2023-05-08 12:38:38 +0530

    feat: add progress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,13 @@ import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import { theme } from "@/services/chakra";
 import { RecoilRoot } from "recoil";
+import NextNProgress from "nextjs-progressbar";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <ChakraProvider theme={theme}>
+        <NextNProgress />
         <Component {...pageProps} />
       </ChakraProvider>
     </RecoilRoot>
